5466ccddba323aaece4769e842ebfb2f77bd77ec
fvknk97034 <97640891+fvknk97034@users.noreply.github.com>
2026-04-08 20:40:30 +0900

    fix: introduce options for furigana comparison and sorting methods

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,12 @@
 FROM ruby:3.4.2
 
+WORKDIR /tmp
+
+RUN apt-get update -qq && apt-get install -y mecab mecab-ipadic-utf8 libmecab-dev sudo
+RUN git clone --depth 1 https://github.com/neologd/mecab-ipadic-neologd.git
+RUN cd mecab-ipadic-neologd && ./bin/install-mecab-ipadic-neologd -n -y
+
 WORKDIR /app
-
-RUN apt-get update -qq && apt-get install -y mecab mecab-ipadic-utf8 libmecab-dev
-
 COPY . .
 
 RUN bundle install
--- lib/jisho_sort/comparable.rb
+++ lib/jisho_sort/comparable.rb
@@ -4,10 +4,10 @@ module JishoSort
     #
     # @param other [String] The object to compare with.
     # @return [Integer] Returns -1 if the current object's furigana is less than the other's, 0 if they are equal, and 1 if it is greater.
-    def compare_by_furigana(other)
+    def compare_by_furigana(other, options = {})
       raise ArgumentError unless other.instance_of?(String)
 
-      furigana <=> other.furigana
+      furigana(options) <=> other.furigana(options)
     end
   end
 end
